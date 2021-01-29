cbf3195f1a1628741c7c02083ee892ba1fc02121
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-29 17:26:05 +0700

    Update xp.js

--- database/menu/xp.js
+++ database/menu/xp.js
@@ -1,24 +1,38 @@
 const xp = (prefix, botName, ownerName) => {
         return `
-「 *${botName}* 」
-
-◪ *INFO*
-  ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${ownerName}
-  ❏ Version: 0.0.4
-◪ *ABOUT*
-  │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
-◪ *XP*
-  │
-  ├─ ❏ ${prefix}leveling
-  ├─ ❏ ${prefix}level
-  └─ ❏ ${prefix}mining
+┏ *〈 ${botName} 〉*
+╿
+┷┯ *〈 BOT INFO 〉*
+   ╽
+   ┠≽ *Prefix* : 「  ${prefix}  」
+   ┠≽ *Creator* : ${ownerName}
+   ┠≽ *Version* : 0.0.4
+   ╿
+┯┷ *〈 ABOUT 〉*
+╽
+┠≽ *${prefix}info* (error)
+┃ *Desc* : Show Bot Details
+┠──────────────╼
+┠≽ *${prefix}blocklist*
+┃ *Desc* : Show Blocked User
+┠──────────────╼
+┠≽ *${prefix}chatlist* (error)
+┃ *Desc* : Show All Chat User
+┠──────────────╼
+┠≽ *${prefix}ping*
+┃ *Desc* : Show Connection Bot Speed
+┠──────────────╼
+┠≽ *${prefix}bugreport* <text>
+┃ *Desc* : Report Bug To Owner Bot
+╿
+┷┯ *〈 LEVEL 〉*
+   ╽
+   ┠≽ *${prefix}level*
+   ┠≽ *${prefix}leveling* <1/0>
+   ┠≽ *${prefix}mining*
+   ╿ *${ownerName}*,
+   ╰╼≽ *Developer © ${botName}
 --------------------------------
-Note: Kamu bisa mengumpulkan xp dan menaikan levelmu dengan cara chat dengan siapapun di dalam grup yang telah di aktifkan fitur leveling nya.`
+Note: Kamu bisa mengumpulkan xp dan menaikan levelmu dengan cara chat dengan siapapun di dalam grup yang telah di aktifkan fitur leveling.`
 }
 exports.xp = xp
