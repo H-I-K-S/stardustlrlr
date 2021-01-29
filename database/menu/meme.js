2dbc491bbfcfa269d8d6ccad311078c94797c9e8
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-29 16:10:21 +0700

    Update meme.js

--- database/menu/meme.js
+++ database/menu/meme.js
@@ -1,21 +1,37 @@
 const meme = (prefix, botName, ownerName) => {
 	return `
-「 *${botName}* 」
-
-◪ *INFO*
-  ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${ownerName}
-  ❏ Version: 0.0.4
-◪ *ABOUT*
-  │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
-◪ *MEME*
-  │
-  ├─ ❏ ${prefix}meme
-  └─ ❏ ${prefix}memeindo`
+┏ *〈 ${botName} 〉*
+╿
+┷┯ *〈 BOT INFO 〉*
+   ╽
+   ┠≽ *Prefix* : 「  ${prefix}  」
+   ┠≽ *Creator* : ${ownerName}
+   ┠≽ *Version* : 0.0.4
+   ╿
+┯┷ *〈 ABOUT 〉*
+╽
+┠≽ *${prefix}info* (error)
+┃ *Desc* : Show Bot Details
+┠──────────────╼
+┠≽ *${prefix}blocklist*
+┃ *Desc* : Show Blocked User
+┠──────────────╼
+┠≽ *${prefix}chatlist* (error)
+┃ *Desc* : Show All Chat User
+┠──────────────╼
+┠≽ *${prefix}ping*
+┃ *Desc* : Show Connection Bot Speed
+┠──────────────╼
+┠≽ *${prefix}bugreport* <text>
+┃ *Desc* : Report Bug To Owner Bot
+╿
+┷┯ *〈 MEME 〉*
+   ╽
+   ┠≽ *${prefix}meme*
+   ┃ *Desc* : Send Random Meme[eng]
+   ┠≽ *${prefix}memeindo*
+   ┃ *Desc* : Send Random Meme [indo]
+   ╿ *${ownerName}*,
+   ╰╼≽ *Developer © ${botName}`
 }
 exports.meme = meme
