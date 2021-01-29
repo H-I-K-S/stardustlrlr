e9ccdf46593bd8dc8b57712380896604094c0875
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-29 16:38:30 +0700

    Update search.js

--- database/menu/search.js
+++ database/menu/search.js
@@ -1,25 +1,41 @@
 const search = (prefix, botName, ownerName) => {
 	return `
-「 *${botName}* 」
-
-◪ *INFO*
-  ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${ownerName}
-  ❏ Version: 0.0.4
-◪ *ABOUT*
-  │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
-◪ *Search*
-  │
-  ├─ ❏ ${prefix}wait
-  ├─ ❏ ${prefix}ytsearch
-  ├─ ❏ ${prefix}trendtwit
-  ├─ ❏ ${prefix}wikien
-  ├─ ❏ ${prefix}wiki
-  └─ ❏ ${prefix}neonime`
+┏ *〈 ${botName} 〉*
+╿
+┷┯ *〈 BOT INFO 〉*
+   ╽
+   ┠≽ *Prefix* : 「  ${prefix}  」
+   ┠≽ *Creator* : ${ownerName}
+   ┠≽ *Version* : 0.0.4
+   ╿
+┯┷ *〈 ABOUT 〉*
+╽
+┠≽ *${prefix}info* (error)
+┃ *Desc* : Show Bot Details
+┠──────────────╼
+┠≽ *${prefix}blocklist*
+┃ *Desc* : Show Blocked User
+┠──────────────╼
+┠≽ *${prefix}chatlist* (error)
+┃ *Desc* : Show All Chat User
+┠──────────────╼
+┠≽ *${prefix}ping*
+┃ *Desc* : Show Connection Bot Speed
+┠──────────────╼
+┠≽ *${prefix}bugreport* <text>
+┃ *Desc* : Report Bug To Owner Bot
+╿
+┷┯ *〈 SEARCH 〉*
+   ╽
+   ┠≽ *${prefix}wait
+   ┃ *Desc* : Send Name Anime & Short Scene
+   ┠──────────────╼
+   ┠≽ *${prefix}ytsearch <query>
+   ┃ *Desc* : Search YouTube Video
+   ┠──────────────╼
+   ┠≽ *${prefix}trendtwit
+   ┃ *Desc* : Search Trending Twitter Video
+   ╿ *${ownerName}*,
+   ╰╼≽ *Developer © ${botName}`
 }
 exports.search = search
