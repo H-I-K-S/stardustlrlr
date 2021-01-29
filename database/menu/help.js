7d7bd3606de0b16b8178ed0a9e863ccbbf818586
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-29 14:43:22 +0700

    Update help.js

--- database/menu/help.js
+++ database/menu/help.js
@@ -16,29 +16,29 @@ const help = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
 ┠≽ *Version* : 0.0.4
 ╿
 ┷┯ *〈 MENU 〉*
-   ┠≽ ${prefix}logomakermenu
-   ┠≽ ${prefix}imagemakermenu
-   ┠≽ ${prefix}stickermakermenu
-   ┠≽ ${prefix}searchmenu
-   ┠≽ ${prefix}educationmenu
-   ┠≽ ${prefix}kerangmenu
-   ┠≽ ${prefix}downloadermenu
-   ┠≽ ${prefix}mememenu
-   ┠≽ ${prefix}groupmenu
-   ┠≽ ${prefix}soundmenu
-   ┠≽ ${prefix}musicmenu
-   ┠≽ ${prefix}islammenu
-   ┠≽ ${prefix}stalkmenu
-   ┠≽ ${prefix}wibumenu
-   ┠≽ ${prefix}18+menu
-   ┠≽ ${prefix}funmenu
-   ┠≽ ${prefix}todmenu
-   ┠≽ ${prefix}informationmenu
-   ┠≽ ${prefix}stayonscreenmenu
-   ┠≽ ${prefix}xpmenu
-   ┠≽ ${prefix}limitmenu
-   ┠≽ ${prefix}ownermenu
-   ┠≽ ${prefix}othermenu
+   ┠≽ *${prefix}logomakermenu*
+   ┠≽ *${prefix}imagemakermenu*
+   ┠≽ *${prefix}stickermakermenu*
+   ┠≽ *${prefix}searchmenu*
+   ┠≽ *${prefix}educationmenu*
+   ┠≽ *${prefix}kerangmenu*
+   ┠≽ *${prefix}downloadermenu*
+   ┠≽ *${prefix}mememenu*
+   ┠≽ *${prefix}groupmenu*
+   ┠≽ *${prefix}soundmenu*
+   ┠≽ *${prefix}musicmenu*
+   ┠≽ *${prefix}islammenu*
+   ┠≽ *${prefix}stalkmenu*
+   ┠≽ *${prefix}wibumenu*
+   ┠≽ *${prefix}18+menu*
+   ┠≽ *${prefix}funmenu*
+   ┠≽ *${prefix}todmenu*
+   ┠≽ *${prefix}informationmenu*
+   ┠≽ *${prefix}stayonscreenmenu*
+   ┠≽ *${prefix}xpmenu*
+   ┠≽ *${prefix}limitmenu*
+   ┠≽ *${prefix}ownermenu*
+   ┠≽ *${prefix}othermenu*
    ╿ *${ownerName}*,
    ╰╼≽ *Developer © ${botName}`
 }
