471309aa4670e1caa8dd9050a39348e3dd218056
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-29 17:01:12 +0700

    Update stickermaker.js

--- database/menu/stickermaker.js
+++ database/menu/stickermaker.js
@@ -1,22 +1,41 @@
 const stickermaker = (prefix, botName, ownerName) => {
 	return `
-「 *${botName}* 」
-
-◪ *INFO*
-  ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${ownerName}
-  ❏ Version: 0.0.4
-◪ *ABOUT*
-  │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
-◪ *STICKER MAKER*
-  │
-  ├─ ❏ ${prefix}sticker
-  ├─ ❏ ${prefix}stickergif
-  └─ ❏ ${prefix}ttp`
+┏ *〈 ${botName} 〉*
+╿
+┷┯ *〈 BOT INFO 〉*
+   ╽
+   ┠≽ *Prefix* : 「  ${prefix}  」
+   ┠≽ *Creator* : ${ownerName}
+   ┠≽ *Version* : 0.0.4
+   ╿
+┯┷ *〈 ABOUT 〉*
+╽
+┠≽ *${prefix}info* (error)
+┃ *Desc* : Show Bot Details
+┠──────────────╼
+┠≽ *${prefix}blocklist*
+┃ *Desc* : Show Blocked User
+┠──────────────╼
+┠≽ *${prefix}chatlist* (error)
+┃ *Desc* : Show All Chat User
+┠──────────────╼
+┠≽ *${prefix}ping*
+┃ *Desc* : Show Connection Bot Speed
+┠──────────────╼
+┠≽ *${prefix}bugreport* <text>
+┃ *Desc* : Report Bug To Owner Bot
+╿
+┷┯ *〈 STICKER MAKER 〉*
+   ╽
+   ┠≽ *${prefix}sticker*
+   ┃ *Desc* : Make Sticker Image
+   ┠──────────────╼
+   ┠≽ *${prefix}stickergif*
+   ┃ *Desc* : Make Sticker Gif/Video
+   ┠──────────────╼
+   ┠≽ *${prefix}ttp*
+   ┃ *Desc* : Text To Sticker
+   ╿ *${ownerName}*,
+   ╰╼≽ *Developer © ${botName}`
 }
 exports.stickermaker = stickermaker
