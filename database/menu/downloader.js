68b46aa070f73b3f79311aa78d9f779b72f54ce2
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-29 11:03:29 +0700

    Update downloader.js

--- database/menu/downloader.js
+++ database/menu/downloader.js
@@ -1,23 +1,44 @@
 const downloader = (prefix, botName, ownerName) => {
 	return `
-「 *${botName}* 」
-
-◪ *INFO*
-  ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${ownerName}
-  ❏ Version: 0.0.4
-◪ *ABOUT*
-  │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
-◪ *DOWNLOADER*
-  │
-  ├─ ❏ ${prefix}pinterest
-  ├─ ❏ ${prefix}ytmp3
-  ├─ ❏ ${prefix}ytmp4
-  └─ ❏ ${prefix}tiktok`
+┏ *〈 ${botName} 〉*
+╿
+┷┯ *〈 BOT INFO 〉*
+   ╽
+   ┠≽ *Prefix* : 「  ${prefix}  」
+   ┠≽ *Creator* : ${ownerName}
+   ┠≽ *Version* : 0.0.4
+   ╿
+┯┷ *〈 ABOUT 〉*
+╽
+┠≽ *${prefix}info* (error)
+┃ *Desc* : Show Bot Details
+┠──────────────╼
+┠≽ *${prefix}blocklist*
+┃ *Desc* : Show Blocked User
+┠──────────────╼
+┠≽ *${prefix}chatlist* (error)
+┃ *Desc* : Show All Chat User
+┠──────────────╼
+┠≽ *${prefix}ping*
+┃ *Desc* : Show Connection Bot Speed
+┠──────────────╼
+┠≽ *${prefix}bugreport* <text>
+┃ *Desc* : Report Bug To Owner Bot
+╿
+┷┯ *〈 DOWNLOADER 〉*
+   ╽
+   ┠≽ *${prefix}pinterest* <query>
+   ┃ *Desc* : Download Image From Pinterest
+   ┠──────────────╼
+   ┠≽ *${prefix}ytmp3* <linkyt>
+   ┃ *Desc* : Download Youtube Video to Mp3
+   ┠──────────────╼
+   ┠≽ *${prefix}ytmp4* <linkyt>
+   ┃ *Desc* : Download Youtube Video
+   ┠──────────────╼
+   ┠≽ *${prefix}tiktok* <linktiktok>
+   ┃ *Desc* : Download TikTok Video
+   ╿ *${ownerName}*,
+   ╰╼≽ *Developer © ${botName}`
 }
 exports.downloader = downloader
