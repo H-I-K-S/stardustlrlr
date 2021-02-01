d13c88fa258334581d41de37447ea5d49802125f
Rvnxpl12 <78360789+rvnzxpl@users.noreply.github.com>
2021-02-01 22:25:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,10 @@ apt-get install ffmpeg
 apt-get install wget
 apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-npm audit fix
 npm i imgbb-uploader
 npm cache clean -f
-npm install --dev
+npm install
 npm i got
 
 echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
-echo "Updates : fix Bugs"
+echo "Updates : fix menu"
