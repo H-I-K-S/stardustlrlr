e97cb150ebf71bdabcd5244d37bc1e81a3c492f6
Rvnxpl12 <78360789+rvnzxpl@users.noreply.github.com>
2021-02-01 22:58:37 +0700

    Update adult.js

--- database/menu/adult.js
+++ database/menu/adult.js
@@ -6,7 +6,7 @@ const adult = (prefix, botName, ownerName) => {
    ╽
    ┠≽ *Prefix* : 「  ${prefix}  」
    ┠≽ *Creator* : ${ownerName}
-   ┠≽ *Version* : 0.0.4
+   ┠≽ *Version* : 0.0.5
    ╿
 ┯┷ *〈 ABOUT 〉*
 ╽
