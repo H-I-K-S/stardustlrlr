56de0561d9ea2ecd4efdb9a314b9eb66e37ddc31
Rvnxpl12 <78360789+rvnzxpl@users.noreply.github.com>
2021-02-01 23:05:56 +0700

    Update logomaker.js

--- database/menu/logomaker.js
+++ database/menu/logomaker.js
@@ -6,7 +6,7 @@ const logomaker = (prefix, botName, ownerName) => {
    ╽
    ┠≽ *Prefix* : 「  ${prefix}  」
    ┠≽ *Creator* : ${ownerName}
-   ┠≽ *Version* : 0.0.4
+   ┠≽ *Version* : 0.0.5
    ╿
 ┯┷ *〈 ABOUT 〉*
 ╽
