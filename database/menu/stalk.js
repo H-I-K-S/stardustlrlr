97d9536f1f74c1484f897e9dbafbc551d309afad
Rvnxpl12 <78360789+rvnzxpl@users.noreply.github.com>
2021-02-01 23:09:57 +0700

    Update stalk.js

--- database/menu/stalk.js
+++ database/menu/stalk.js
@@ -6,7 +6,7 @@ const stalk = (prefix, botName, ownerName) => {
    ╽
    ┠≽ *Prefix* : 「  ${prefix}  」
    ┠≽ *Creator* : ${ownerName}
-   ┠≽ *Version* : 0.0.4
+   ┠≽ *Version* : 0.0.5
    ╿
 ┯┷ *〈 ABOUT 〉*
 ╽
